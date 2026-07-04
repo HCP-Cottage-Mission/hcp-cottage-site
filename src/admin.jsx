244845db4d3db836c43a3ee9523955dfed76a6f5
Keith R. Harris <harriskr@gmail.com>
2026-07-04 00:40:10 -0400

    fix: Correct CSS import path in admin.jsx

--- src/admin.jsx
+++ src/admin.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AdminApp from './AdminApp.jsx'
-import '../index.css'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('admin-root')).render(
   <React.StrictMode>
